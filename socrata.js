ceecab27464986c0309920ccafd229a7ebd6f52a
unknown <Laura@.(none)>
2014-04-05 11:47:51 -0400

    merging l with k's work

--- socrata.js
+++ socrata.js
@@ -1,9 +1,23 @@
 var token = "nYkGKGozNTZZ4npCsN43ciFpx";
 var url = "https://opendata.socrata.com/resource/2e9u-3gji.json?$select=location";
 
+
+window.onload = function(){
+var map = L.map('map').setView([39.28,-76.61],15);
+alert('hi');
+L.tileLayer('http://{s}.tile.cloudmade.com/008c020e60c4426fba424183cd542f23/125441/256/{z}/{x}/{y}.png', {
+attribution: 'Baltimore Map Trial 1.1',
+maxZoom: 20
+	}).addTo(map)
+
+L.marker([39.28326056920309, -76.59904929010796]).addTo(map)
+.bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+.openPopup();
+
 $.getJSON( url, function( data ) {
 	  var items = [];
 	  $.each( data, function( key, val ) {
+		  L.marker([val.location.latitude, val.location.longitude]).addTo(map)
 	    items.push( "<li id='" + key + "'>" + val.location.longitude + "</li>" );
 	  });
 	 
@@ -12,3 +26,4 @@ $.getJSON( url, function( data ) {
 	    html: items.join( "" )
 	  }).appendTo( "body" );
 	});
+}
