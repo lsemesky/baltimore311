9063c06837f2b41f32e845f9f1d9ce1a95b9c490
Kajari Ghosh <kghosh1@students.towson.edu>
2014-05-12 15:35:58 -0400

    RenamedFilters

--- socrata.js
+++ socrata.js
@@ -9,14 +9,14 @@ var SW = new L.MarkerClusterGroup({
 		disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-blue' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-blue' , iconSize: new L.Point(40, 40) });
 	}
 });
 var HCD = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-pink' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-pink' , iconSize: new L.Point(40, 40) });
 	}
 
 });
@@ -24,7 +24,7 @@ var TRM = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-black' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-black' , iconSize: new L.Point(40, 40) });
 	}
 
 });
@@ -32,7 +32,7 @@ var TRS = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-fuscha' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-fuscha' , iconSize: new L.Point(40, 40) });
 	}
 
 });
@@ -40,21 +40,21 @@ var WW = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-teal' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-teal' , iconSize: new L.Point(40, 40) });
 	}
 });
 var TRT = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-yellow' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-yellow' , iconSize: new L.Point(40, 40) });
 	}
 });
 var HLTH = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-purple' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-purple' , iconSize: new L.Point(40, 40) });
 	}
 });
 var BCLB = new L.MarkerClusterGroup({
@@ -79,7 +79,7 @@ var Forestry = new L.MarkerClusterGroup({
 	disableClusteringAtZoom: 18,
 	iconCreateFunction: function (cluster) {
 		var childCount = cluster.getChildCount();
-		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-green' , iconSize: new L.Point(40, 40) });	
+		return new L.DivIcon({ html: '<div><span>' + childCount + '</span></div>', className: 'marker-cluster marker-cluster-green' , iconSize: new L.Point(40, 40) });
 	}
 
 });
@@ -104,12 +104,12 @@ window.onload = function(){
 	map = L.map('map').setView([39.28,-76.61],11);
 
 	L.tileLayer('http://a.tiles.mapbox.com/v3/lsemesky.i7ila685/{z}/{x}/{y}.png', {
-	
+
 	attribution: '311 Service Calls Baltimore',
 	maxZoom: 18
 	}).addTo(map)
 
-	
+
 
 	//getMarkers(location_url);
 	generateDescriptions();
@@ -118,44 +118,45 @@ window.onload = function(){
 
 }
 
-function generateDescriptions(){urls.push("SW - Sanitation & Waste");	
-urls.push("Forestry - Forestry Services");
-urls.push("HCD - Housing & Community Development");
-urls.push("TRM - Transportation & Roadway Maintenance");
-urls.push("TRS - Transportation & Roadway Service");
-urls.push("WW - Water & Wastewater");
-urls.push("TRT - Transportation & Roadway ");
-urls.push("HLTH - Health");
-urls.push("BCLB - Baltimore City Liquor");
-urls.push("ECC - Roadside Maintenance");
-urls.push("FCCS - Financial Complaints");
+function generateDescriptions(){
+	urls.push("Sanitation & Waste");
+	urls.push("Forestry - Forestry Services");
+	urls.push("Housing & Community Development");
+	urls.push("Transportation & Roadway Maintenance");
+	urls.push("Transportation & Roadway Service");
+	urls.push("Water & Wastewater");
+	urls.push("Transportation & Roadway ");
+	urls.push("Health");
+	urls.push("Baltimore City Liquor");
+	urls.push("Roadside Maintenance");
+	urls.push("Financial Complaints");
 }
 
 function setLayers(string) {
 switch(string){
 case "":
-case "SW - Sanitation & Waste": var SW_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'SW')";
-currentIcon = new L.Icon.Default();getMarkers(SW_url, SW,currentIcon); 
-case "Forestry - Forestry Services":var Forest_url = base_url + "&$select=codedescription,location,address&$where=starts_with(codedescription,'Forestry')";
+case "Sanitation & Waste": var SW_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'SW')";
+currentIcon = new L.Icon.Default();getMarkers(SW_url, SW,currentIcon);
+case "Forestry Services":var Forest_url = base_url + "&$select=codedescription,location,address&$where=starts_with(codedescription,'Forestry')";
 currentIcon = greenIcon;getMarkers(Forest_url, Forestry,currentIcon);
-case "HCD - Housing & Community Development":var HCD_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'HCD')";
+case "Housing & Community Development":var HCD_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'HCD')";
 currentIcon = pinkIcon;getMarkers(HCD_url, HCD,currentIcon);
-case "TRM - Transportation & Roadway Maintenance":var TRM_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRM')";
+case "Transportation & Roadway Maintenance":var TRM_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRM')";
 currentIcon = blackIcon;getMarkers(TRM_url, TRM,currentIcon);
-case "TRS - Transportation & Roadway Service": var TRS_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRS')";
-currentIcon = fuschaIcon;getMarkers(TRS_url, TRS,currentIcon);	
-case "WW - Water & Wastewater":var WW_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'WW')";
+case "Transportation & Roadway Service": var TRS_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRS')";
+currentIcon = fuschaIcon;getMarkers(TRS_url, TRS,currentIcon);
+case "Water & Wastewater":var WW_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'WW')";
 currentIcon = tealIcon;getMarkers(WW_url, WW,currentIcon);
-case "TRT - Transportation & Roadway ":var TRT_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRT')";
+case "Transportation & Roadway ":var TRT_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'TRT')";
 currentIcon = yellowIcon;getMarkers(TRT_url, TRT,currentIcon);
-case "HLTH - Health":var HLTH_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'HLTH')";
+case "Health":var HLTH_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'HLTH')";
 currentIcon = purpleIcon;getMarkers(HLTH_url, HLTH,currentIcon);
-case "BCLB - Baltimore City Liquor":	var BCLB_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'BCLB')";
+case "Baltimore City Liquor":	var BCLB_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'BCLB')";
 currentIcon = redIcon;getMarkers(BCLB_url, BCLB,currentIcon);
-	
+
 case "ECC - Roadside Maintenance":var ECC_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'ECC')";
 currentIcon = brownIcon;	getMarkers(ECC_url, ECC,currentIcon);
-	
+
 case "FCCS - Financial Complaints":var FCCS_url = base_url +"&$select=codedescription,location,address&$where=starts_with(codedescription,'FCCS')";
 currentIcon = ochreIcon;getMarkers(FCCS_url, FCCS,currentIcon);
 default: return;
@@ -182,12 +183,12 @@ function getLayers(string) {
 
 
 function getMarkers(url, layer,icon) {
-	
-	
+
+
 	$.getJSON( url, function( data ) {
 		  var items = [];
 		  $.each( data, function( key, val ) {
-			  
+
 			items.push =  L.marker([val.location.latitude, val.location.longitude], {icon: icon}).addTo(layer).bindPopup(val.address+ "<br />" + val.codedescription).on('mouseover', function(evt) {
 				  evt.target.openPopup();
 			 }).on('mouseout', function(evt) {
@@ -203,15 +204,15 @@ function getIconColor(layer){
 	switch(layer){
 	case SW: return tealIcon;
 	case Forestry: return greenIcon;
-	case HCD: return brownIcon; 
+	case HCD: return brownIcon;
 	case TRM: return redIcon;
-	case TRS: return yellowIcon; 
-	case WW: return new L.Icon.Default(); 
-	case TRT: return pinkIcon; 
+	case TRS: return yellowIcon;
+	case WW: return new L.Icon.Default();
+	case TRT: return pinkIcon;
 	case HLTH: return ochreIcon;
-	case BCLB: return fuschaIcon; 
-	case ECC: return blackIcon; 
-	case FCCS: return purpleIcon; 
+	case BCLB: return fuschaIcon;
+	case ECC: return blackIcon;
+	case FCCS: return purpleIcon;
 	default: return new L.Icon.Default();
 	}
 }
@@ -220,12 +221,12 @@ function getIconColor(layer){
 function setCheckBoxes() {
 	var items = [];
 $.each(urls,function(key,val){
-	
+
 			var str = "<input type='checkbox' onclick='getCheckBoxes();' id='" + key + "' value = '" + val + "' />" + val + "<br>";
 		    items.push(str);
 		  });
-		 
-		  $("<div/>", { 
+
+		  $("<div/>", {
 			"id": "CheckList",
 			 html: items.join( "" )
 		  }).appendTo("#sidebar");
@@ -240,14 +241,14 @@ function getCheckBoxes() {
 		l = getLayers(val.value);
 		if(!map.hasLayer(l)){
 			map.addLayer(l);
-			
+
 		}
-		
+
 		});
 	var u = $("input:checkbox:not(:checked)");
 	$.each(u, function(key,val){
 		l = getLayers(val.value);
-		
+
 		if(map.hasLayer(l)){
 			map.removeLayer(l);
 		}
